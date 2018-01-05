d872a35f672858ff1976e2fe0b9ab648c8674aa7
DisorderSec <messiassouzakb@gmail.com>
2018-01-05 16:53:16 -0500

    Update and rename install.sh to installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 wget --no-check-certificate https://raw.githubusercontent.com/RememberTheAir/GroupButler/master/install.sh && bash install.sh
-    
 echo -e "\e[1;32mSuccessfully installed!, change values in config file and run ./launch.sh\e[0m"
