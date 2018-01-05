b38ddc5870be3d113a819049e98764b12b0de89c
DisorderSec <messiassouzakb@gmail.com>
2018-01-05 16:52:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-
-echo -e "\e[1;36mUpdating packages\e[0m"
-sudo apt-get update -y
-
-echo -e "\e[1;36mInstalling dependencies\e[0m"
-sudo apt-get install libreadline-dev libssl-dev lua5.2 luarocks liblua5.2-dev git make unzip redis-server curl libcurl4-gnutls-dev -y
-    
-echo -e "\e[1;36mInstalling rocks\e[0m"
-sudo luarocks install luasocket
-sudo luarocks install luasec
-sudo luarocks install redis-lua
-sudo luarocks install lua-term
-sudo luarocks install serpent
-sudo luarocks install dkjson
-sudo luarocks install lanes
-sudo luarocks install Lua-cURL
+wget --no-check-certificate https://raw.githubusercontent.com/RememberTheAir/GroupButler/master/install.sh && bash install.sh
     
 echo -e "\e[1;32mSuccessfully installed!, change values in config file and run ./launch.sh\e[0m"
